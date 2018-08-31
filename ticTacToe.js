45ba33e7dfc90c4c2b09830a017a2a7cfc0df8d7
Sacha <sachau888@hotmail.com>
2018-08-31 16:16:22 +0200

    add checkWin constant function to ticTacToe.js

--- ticTacToe.js
+++ ticTacToe.js
@@ -21,15 +21,43 @@ const render = function(e){
 		input = playerOne.symbol;
 		gameBoard.turn++;
 		gameBoard.board[position] = input;
+		checkWin(playerOne);
 	} else if((gameBoard.turn % 2 != 0) && (gameBoard.board[position] === undefined) && (playerTwo.win ==='no')){
 		input = playerTwo.symbol;
 		gameBoard.turn++;
 		gameBoard.board[position] = input;
+		checkWin(playerTwo);
 	}
 	let index = 0;
 	for(let i of gameBoard.board){
 		document.getElementsByClassName('content')[index].textContent = i;
 		index++;
+	}
+}
+
+const checkWin = function(playerObj){
+	for (let i=0, j=0; i<6, j<3; i=i+3, j++){
+		let rowCheck = [gameBoard.board[i],gameBoard.board[i+1],gameBoard.board[i+2]];
+		let colCheck = [gameBoard.board[j],gameBoard.board[j+3],gameBoard.board[j+6]];
+		let diagDownCheck = [gameBoard.board[i+0],gameBoard.board[i+4],gameBoard.board[i+8]];
+		let diagUpCheck = [gameBoard.board[i+2],gameBoard.board[i+4],gameBoard.board[i+6]];
+		console.log(i);
+		console.log(diagDownCheck);
+		if(rowCheck.every(x => x === playerObj.symbol)){
+			console.log(playerObj.name + " win");
+			playerObj.win = "yes";
+		} else if(colCheck.every(x => x === playerObj.symbol)){
+			console.log(playerObj.name + " win");
+			playerObj.win = "yes";
+		} else if(diagDownCheck.every(x => x === playerObj.symbol)){
+			console.log(playerObj.name + " win");
+			playerObj.win = "yes";
+		} else if(diagUpCheck.every(x => x === playerObj.symbol)){
+			console.log(playerObj.name + " win");
+			playerObj.win = "yes";
+		} else if((gameBoard.turn === 9) && (playerObj.win === 'no')){
+			console.log('tie');
+		}
 	}
 }
 
